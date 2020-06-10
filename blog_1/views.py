8286187bd29ca1d007605cea3978dddc3cb70053
Hanlin Miao <hanlin980505@gmail.com>
2020-06-10 19:56:59 -0400

    added blogpost model and register the model to the Django admin

--- blog_1/views.py
+++ blog_1/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from . models import blogpost
 
-posts =[
-    {
-    'author' : 'Hanlin Miao',
-    'title': 'Blog Post 1',
-    'content': 'First Post content',
-    'date_posted': '6/9/2020'
-    },
-    {
-    'author' : 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'second Post content',
-    'date_posted': '6/10/2020'
-    },
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': blogpost.objects.all()
     }
     return render(request, 'blog_1/home.html', context)
 
