ef77e4392ed05cfba3e80bac1d00f9706dd0b7cc
Hanlin Miao <hanlin980505@gmail.com>
2020-06-10 18:04:21 -0400

    Added bootstrap starter templates and some CSS files

--- blog_1/views.py
+++ blog_1/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts =[
+    {
+    'author' : 'Hanlin Miao',
+    'title': 'Blog Post 1',
+    'content': 'First Post content',
+    'date_posted': '6/9/2020'
+    },
+    {
+    'author' : 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'second Post content',
+    'date_posted': '6/10/2020'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog_1/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog_1/about.html', {'title': 'About'})
+
--- blogger/urls.py
+++ blogger/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog_1.urls')),
+    path('', include('blog_1.urls')),
     
 ]
